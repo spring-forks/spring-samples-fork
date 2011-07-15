384c7e5ff6fd3a2a3384ca7dc39901a9dd07731a
Josh Long <josh.long@springsource.com>
2011-07-15 14:17:01 -0700

    updated the code again -- more tests

--- showcases/spring-a-walking-tour/web/mvc/src/main/java/org/springsource/examples/sawt/web/mvc/WebConfig.java
+++ showcases/spring-a-walking-tour/web/mvc/src/main/java/org/springsource/examples/sawt/web/mvc/WebConfig.java
@@ -3,20 +3,20 @@ package org.springsource.examples.sawt.web.mvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springsource.examples.sawt.services.jdbc.Config;
 
 @Configuration
-@Import( org.springsource.examples.sawt.web.mvc.WebConfig.class)
+@Import(Config.class)
 public class WebConfig {
 
-	@Bean
-	public UrlBasedViewResolver resolver() {
-		UrlBasedViewResolver url = new UrlBasedViewResolver();
-		url.setPrefix("views/");
-		url.setViewClass(JstlView.class);
-		url.setSuffix(".jsp");
-		return url;
-	}
+    @Bean
+    public UrlBasedViewResolver resolver() {
+        UrlBasedViewResolver url = new UrlBasedViewResolver();
+        url.setPrefix("views/");
+        url.setViewClass(JstlView.class);
+        url.setSuffix(".jsp");
+        return url;
+    }
 }
