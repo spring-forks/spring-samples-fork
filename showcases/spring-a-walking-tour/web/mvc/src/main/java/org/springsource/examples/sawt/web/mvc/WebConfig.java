641cee361c47b1fc7a97cb4c48a5e33dba8be57a
Josh Long <josh.long@springsource.com>
2011-07-16 00:55:01 -0700

    massive cleanup moving an more more code to Spring 3.1

--- showcases/spring-a-walking-tour/web/mvc/src/main/java/org/springsource/examples/sawt/web/mvc/WebConfig.java
+++ showcases/spring-a-walking-tour/web/mvc/src/main/java/org/springsource/examples/sawt/web/mvc/WebConfig.java
@@ -3,13 +3,17 @@ package org.springsource.examples.sawt.web.mvc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springsource.examples.sawt.services.jdbc.Config;
 
 @Configuration
+@EnableWebMvc
 @Import(Config.class)
-public class WebConfig {
+public class WebConfig  extends WebMvcConfigurerAdapter{
 
     @Bean
     public UrlBasedViewResolver resolver() {
@@ -18,5 +22,12 @@ public class WebConfig {
         url.setViewClass(JstlView.class);
         url.setSuffix(".jsp");
         return url;
+    }
+
+    @Override
+    public void configureViewControllers(ViewControllerConfigurer configurer) {
+//        <mvc:view-controller path="/" view-name="welcome"/>
+        configurer.mapViewName("/", "welcome") ;
+
     }
 }
--- showcases/spring-a-walking-tour/web/rest/src/main/java/org/springsource/examples/sawt/web/rest/service/WebConfig.java
+++ showcases/spring-a-walking-tour/web/rest/src/main/java/org/springsource/examples/sawt/web/rest/service/WebConfig.java
@@ -1,10 +1,14 @@
 package org.springsource.examples.sawt.web.rest.service;
 
- import org.springframework.context.annotation.Configuration;
- import org.springframework.context.annotation.Import;
- import org.springsource.examples.sawt.services.jdbc.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springsource.examples.sawt.services.jdbc.Config;
 
-@Configuration
+@EnableWebMvc
 @Import(Config.class)
-public class WebConfig {
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
 }
